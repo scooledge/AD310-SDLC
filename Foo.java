86b837455ca73ee64656d95482cfcdafecaa9414
Sam Cooledge <scooledge@gmail.com>
2015-11-26 09:29:03 -0800

    added original login

---
+++ Foo.java
@@ -0,0 +1,11 @@
+
+
+public class Foo {
+  
+  public static void main(String[] args) {
+    
+    System.out.println("I am Foo");
+    
+  }
+  
+}  
--- login/login/src/login/Login.java
+++ login/login/src/login/Login.java
@@ -5,11 +5,11 @@
  */
 package login;
 
-import java.util.Scanner;
+import java.awt.event.WindowEvent;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
-
+import javafx.event.EventType;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
@@ -18,7 +18,9 @@ import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
-
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 /**
@@ -27,24 +29,6 @@ import javafx.stage.Stage;
  */
 public class Login extends Application {
     
-    private Scanner scan = new Scanner(System.in);
-    
-    
-    
-    //private String word;
-    
-    private char[] array = new char[100];
-    
-    public Login() {
-        for(int i = 0 ; i < 100 ; i++){
-            array[i] = ' ';
-        }
-        
-        
-        
-        
-    }
-   
     @Override
     public void start(Stage primaryStage) {
         primaryStage.setTitle("Crouching Tiger Hidden Battleship");
@@ -56,30 +40,36 @@ public class Login extends Application {
         grid.setPadding(new Insets(25, 25, 25, 25));
         
         Text scenetitle = new Text("BattleShip");
-        
+        //scenetitle.setFont(Font.font("Tahoma", FontWeight.BOLD, 20));
         scenetitle.setId("welcome-text");
         grid.add(scenetitle, 0, 0, 2, 1);
 
         Label player1 = new Label("Player 1:");
         grid.add(player1, 0, 1);
 
-        player1.getText();
-        
-        TextField p1 = new TextField();
-        grid.add(p1, 1, 1);
+        TextField userTextField = new TextField();
+        grid.add(userTextField, 1, 1);
 
-        //Label player2 = new Label("Player 2:");
         Label player2 = new Label("Player 2:");
+        
         grid.add(player2, 0, 2);
         
         player2.getText();
-         
         
         
-        TextField p2 = new TextField();
-        grid.add(p2, 1, 2);
+        TextField userTextField2 = new TextField();
+        grid.add(userTextField2, 1, 2);
         
-       
+       /* Label label1 = new Label("Name:");
+        TextField textField = new TextField ();
+        HBox hb = new HBox();
+        hb.getChildren().addAll(label1, textField);
+        hb.setSpacing(10);
+        grid.add(label1, 0, 3);
+        grid.add(textField, 1,3);
+        //userTextField2
+    **/
+        
         Button btn = new Button("let's play!");
         
         HBox hbBtn = new HBox(10);
@@ -88,159 +78,64 @@ public class Login extends Application {
         grid.add(hbBtn, 1, 4);
         
         
-      final Text actiontarget = new Text();
+       final Text actiontarget = new Text();
         
-    grid.add(actiontarget, 1, 6);
-    
-    
-    
-   btn.setOnAction(new EventHandler<ActionEvent>() {
-    
+       final Stage bye = new Stage();
+       /*final Stage bye = new Stage();
+        
+        grid.add(actiontarget, 1, 6);
        
-            
-    @Override
-    public void handle(ActionEvent e) {
-        
-        
-        
-        System.out.println("Welcome:" + " " + p1.getText()+" "+"&" + " " + p2.getText());
-        //actiontarget.setFill(Color.FIREBRICK);
-        actiontarget.setId("actiontarget");
-        actiontarget.setText("Welcome" + " " +p1.getText() + " " + "&" + " " + p2.getText());
         
         btn.setOnAction(new EventHandler<ActionEvent>() {
-            @Override
-            public void handle(ActionEvent e) {
-                primaryStage.hide();
-                       
-            }
-        });
-            }
             
-    });
+    @Override 
+    public void handle(ActionEvent e) {
+       
+        bye.hide();
         
-   
+        
+    } */
+//});
+    grid.add(actiontarget, 1, 6);
+    
+        btn.setOnAction(new EventHandler<ActionEvent>() {
+        
+    @Override
+    public void handle(ActionEvent e) {
+        //actiontarget.setFill(Color.FIREBRICK);
+        actiontarget.setId("actiontarget");
+        actiontarget.setText("welcome" + " " +userTextField.getText() + " " + userTextField2.getText() );
+        
+     }
 
-        Scene scene = new Scene(grid, 734, 400);
+});
+        
+btn.setOnAction(new EventHandler<ActionEvent>() {
+        
+    @Override
+    public void handle(ActionEvent e) {
+        //actiontarget.setFill(Color.FIREBRICK);
+        primaryStage.hide();
+        grid.setVisible(false);
+        
+     }
+
+});                  
+
+        Scene scene = new Scene(grid, 500, 350);
         primaryStage.setScene(scene);
         scene.getStylesheets().add
             (Login.class.getResource("login.css").toExternalForm());
         primaryStage.show();
         
     }
-    
-    public String getInput(){
-        
-        System.out.print("Enter location: " );
-        String word = scan.next();
-        System.out.println("You entered " + word);
-        return word;
-        
-    }
-   
-       public void printBoard() {
-        
-        System.out.println("     1   2   3   4   5   6   7   8   9  10");
-        System.out.println("   +---+---+---+---+---+---+---+---+---+---+");
-            for (int row = 0; row < 10; row++) {
-                char rowLabel = (char) ('A' + row);
-                System.out.print(" " + rowLabel + " |");
-            
-                for (int col = 0; col < 10; col++) {
-                   
-                    char value = ' ';
-                    System.out.print(" " + value + " |");
-            
-      }
-            
-      System.out.println(); // end the current line
-      System.out.println("   |---+---+---+---+---+---+---+---+---+---|");
-      
-    }
-     System.out.println();
-      System.out.println();
- }
-      
-    
-   /* public void printBoard(char ships) {
-        
-        System.out.println("     1   2   3   4   5   6   7   8   9  10");
-        System.out.println("   +---+---+---+---+---+---+---+---+---+---+");
-            for (int row = 0; row < 10; row++) {
-                char rowLabel = (char) ('A' + row);
-                System.out.print(" " + rowLabel + " |");
-            
-            for (int col = 0; col < 10; col++) {
-                char value = ' ';
-                System.out.print(" " + value + " |");
-                
-      }
-            
-      System.out.println(); // end the current line
-      System.out.println("   |---+---+---+---+---+---+---+---+---+---|");
-      
-    }
-    
- }*/
-     public void placeShip(){
-         System.out.print("Enter location: " );
-         String word = scan.next();
-         
-         int r = (int)(word.charAt(0)-'A');
-         int c = Integer.parseInt(word.substring(1)) - 1;
-         
-         int ind = r*10 + c;
-         
-         array[ind] = 'D';
-         
-         System.out.println(r +" "+ c);
-         
-         
-         
-              
-        System.out.println("     1   2   3   4   5   6   7   8   9  10");
-        System.out.println("   +---+---+---+---+---+---+---+---+---+---+");
-      
-            for (int row = 0; row < 10; row++) {
-                char rowLabel = (char) ('A' + row);
-                System.out.print(" " + rowLabel + " |");
-           
-                for (int col = 0; col < 10; col++) {
-                    int index = row*10+col;
-                    char value = array[index];
-                    System.out.print(" " + value + " |");
-                
-      }
-            
-      System.out.println(); // end the current line
-      System.out.println("   |---+---+---+---+---+---+---+---+---+---|");
-      
-    
-      }    
-         
-            
-      
-        
-     }
-         
-    
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         launch(args);
-       
-   
-        Login go;
-        go = new Login();
         
-        go.printBoard();
-        //go.getInput();
-        //go.printBoard(ships);
-        while(true){
-        go.placeShip();
-        }
-            
     }
-    
+
+ 
 }
